edc12fbc33069181be1b0a94b7d0729dd3849a7d
WojDrew <38838129+WojDrew@users.noreply.github.com>
2019-05-04 19:46:34 +0200

    Add files via upload

---
+++ crop.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+def crop( inputPath):
+    im = Image.open(inputPath)
+    pixels = list(im.getdata())
+    imgwidth, imgheight = im.size
+    #newwidth = imgwidth/10
+    #k = 0
+    imagesList = []
+    image = []
+
+    for i in range(0,imgwidth):
+        if(i != 0 and i%128 == 0):
+            imagesList.append(image)
+            image = []
+        col = []
+
+        for j in range(i,imgwidth*imgheight - imgwidth + i + 1 ,imgwidth):
+            col.append(pixels[j])
+
+        image.append(col)
+    if(imgwidth%128 == 0):
+        imagesList.append(image)
+    return imagesList
