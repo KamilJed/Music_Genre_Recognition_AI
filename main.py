aca88c7bcc428c14dd83ce538f6e609d4ee247cf
KamilJed <kamijedrzejczak98@wp.pl>
2019-05-31 19:28:05 +0200

    Model finally working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import spectrogramsCreator as sC
 from dataSet import createTrainingSet
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPool2D, Dropout
+from tensorflow.python.keras.optimizers import rmsprop
 from musicLabelBinarizer import fit_trasform
 from musicFileInfo import getAllGenres
 import numpy
@@ -9,8 +10,10 @@ import numpy
 model = Sequential()
 
 model.add(Conv2D(filters=64, kernel_size=2, activation='elu', input_shape=(128, 128, 1)))
+model.add(Conv2D(filters=64, kernel_size=2, activation='elu'))
 model.add(MaxPool2D(2))
 
+model.add(Conv2D(filters=128, kernel_size=2, activation='elu'))
 model.add(Conv2D(filters=128, kernel_size=2, activation='elu'))
 model.add(MaxPool2D(2))
 
@@ -22,14 +25,14 @@ model.add(MaxPool2D(2))
 
 model.add(Flatten())
 model.add(Dense(1024, activation='elu'))
-model.add(Dropout(rate=0.5))
+#model.add(Dropout(rate=0.5))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=rmsprop(lr=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
 
 print(model.get_weights())
 spectrogramsAvalaible = sC.getAvalaibleSpectrograms()
-dataSetTrain = createTrainingSet(spectrogramsAvalaible, 200)
+dataSetTrain = createTrainingSet(spectrogramsAvalaible, 700)
 dataSetVal = createTrainingSet(spectrogramsAvalaible, 200)
 dataSetTrainX, dataSetTrainY = zip(*dataSetTrain)
 dataSetValX, dataSetValY = zip(*dataSetVal)
@@ -43,5 +46,6 @@ validX = numpy.asarray(dataSetValX)
 validX = validX.reshape([-1, 128, 128, 1])
 validY = fit_trasform(dataSetValY, getAllGenres())
 
-model.fit(trainX, trainY, validation_data=(validX, validY), epochs=1, batch_size=128, shuffle=True)
+model.fit(trainX, trainY, validation_data=(validX, validY), epochs=50, batch_size=200, shuffle=True)
 print(model.get_weights())
+model.save("model")
