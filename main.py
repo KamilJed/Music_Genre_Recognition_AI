3395271cf01cd3c91c8399d998011067d2243fe2
KamilJed <kamijedrzejczak98@wp.pl>
2019-06-02 12:45:08 +0200

    Improved data for model training. Model is now even better

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+from tensorflow.python.keras.callbacks import ModelCheckpoint
 import spectrogramsCreator as sC
 from dataSet import createTrainingSet
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPool2D, Dropout
-from tensorflow.python.keras.optimizers import rmsprop
+from tensorflow.python.keras.optimizers import rmsprop, Adam
 from musicLabelBinarizer import fit_trasform
 from musicFileInfo import getAllGenres
+from tensorflow.python.keras.callbacks import TensorBoard
 import numpy
 
 model = Sequential()
 
 model.add(Conv2D(filters=64, kernel_size=2, activation='elu', input_shape=(128, 128, 1)))
-model.add(Conv2D(filters=64, kernel_size=2, activation='elu'))
 model.add(MaxPool2D(2))
 
-model.add(Conv2D(filters=128, kernel_size=2, activation='elu'))
 model.add(Conv2D(filters=128, kernel_size=2, activation='elu'))
 model.add(MaxPool2D(2))
 
@@ -25,17 +25,24 @@ model.add(MaxPool2D(2))
 
 model.add(Flatten())
 model.add(Dense(1024, activation='elu'))
-#model.add(Dropout(rate=0.5))
+model.add(Dropout(rate=0.5))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(optimizer=rmsprop(lr=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(epsilon=1e-08), loss='categorical_crossentropy', metrics=['accuracy'])
 
 print(model.get_weights())
 spectrogramsAvalaible = sC.getAvalaibleSpectrograms()
-dataSetTrain = createTrainingSet(spectrogramsAvalaible, 700)
-dataSetVal = createTrainingSet(spectrogramsAvalaible, 200)
-dataSetTrainX, dataSetTrainY = zip(*dataSetTrain)
-dataSetValX, dataSetValY = zip(*dataSetVal)
+dataSet = createTrainingSet(spectrogramsAvalaible, len(spectrogramsAvalaible))
+dataSetX, dataSetY = zip(*dataSet)
+
+dataSetTrainX = dataSetX[:int(0.7*len(dataSetX))]
+dataSetTrainY = dataSetY[:int(0.7*len(dataSetY))]
+
+dataSetValX = dataSetX[int(0.7*len(dataSetX)):int(0.9*len(dataSetX))]
+dataSetValY = dataSetY[int(0.7*len(dataSetY)):int(0.9*len(dataSetY))]
+
+dataSetTestX = dataSetX[int(0.9*len(dataSetX)):]
+dataSetTestY = dataSetY[int(0.9*len(dataSetY)):]
 
 
 trainX = numpy.asarray(dataSetTrainX)
@@ -46,6 +53,19 @@ validX = numpy.asarray(dataSetValX)
 validX = validX.reshape([-1, 128, 128, 1])
 validY = fit_trasform(dataSetValY, getAllGenres())
 
-model.fit(trainX, trainY, validation_data=(validX, validY), epochs=50, batch_size=200, shuffle=True)
-print(model.get_weights())
-model.save("model")
+testX = numpy.asarray(dataSetTestX)
+testX = testX.reshape([-1, 128, 128, 1])
+testY = fit_trasform(dataSetTestY, getAllGenres())
+
+checkpoint = ModelCheckpoint("best_model_ever_adam", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
+                          write_graph=True, write_images=False)
+callbacks_list = [checkpoint, tensorboard]
+
+model.fit(trainX, trainY, validation_data=(validX, validY), epochs=20, batch_size=128, shuffle=True,
+          callbacks=callbacks_list)
+
+print("model succesfully trained")
+model.save("model_adam")
+
+testAccu = model.evaluate(testX, testY)[0]
